d2b13bcfd35a8aba1e7d6b66bf5d1458d64d84e2
Lucas Pacheco <lucaspsilveira@live.com>
2020-07-21 20:19:20 -0300

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Lucas Pacheco's Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Lucas Pacheco`,
+      summary: `sempre curioso e pensando em novas soluções.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `Repositório de textos sobre minha jornada pela área da tecnologia.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `lucaspsilveiras`,
     },
   },
   plugins: [
